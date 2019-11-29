ed63eb1b48649621fa6405b81bbdd10876ef6084
Rizwan Khan <34640589+khan-rizwan@users.noreply.github.com>
2019-11-29 18:22:49 +0530

    Update app.go

--- app.go
+++ app.go
@@ -149,7 +149,7 @@ func whoamiHandler(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 
-	_, _ = fmt.Fprintln(w, "Dunzo:", "0.1.8")
+	_, _ = fmt.Fprintln(w, "Dunzo:", "0.1.9")
 
 	hostname, _ := os.Hostname()
 	_, _ = fmt.Fprintln(w, "Hostname:", hostname)
